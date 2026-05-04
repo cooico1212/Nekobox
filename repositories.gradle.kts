9361276e12b9da7734fd78c6e8450fa11254fa14
半成梦 <2080300299@qq.com>
2026-05-04 20:04:11 +0800

    完整项目上传 2026-05-04

---
+++ repositories.gradle.kts
@@ -0,0 +1,9 @@
+repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/public/") }
+    maven { url = uri("https://maven.aliyun.com/repository/google/") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+    maven(url = "https://jitpack.io")
+}
